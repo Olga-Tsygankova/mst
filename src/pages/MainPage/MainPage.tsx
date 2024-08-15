0a068be873ddd702618e6e2c7b6884da48dfd5d3
Olga Tsygankova <tsygankova@ispolcom.ru>
2024-08-15 23:06:25 +0300

    feat: добавила MainPage

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,10 @@
+import { Need } from '../../components/Need';
+import styles from './styles.module.css'
+
+export const MainPage = () => {
+  return (
+    <div className={styles.mainPageWrapper}>
+      <Need/>
+    </div>
+  );
+};
