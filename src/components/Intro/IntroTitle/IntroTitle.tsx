402267f8aa642fde8e50d9f12c73f3603b1e948c
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-08-19 13:40:35 +0300

    init: start introTitle styles

--- src/components/Intro/IntroTitle/IntroTitle.tsx
+++ src/components/Intro/IntroTitle/IntroTitle.tsx
@@ -2,7 +2,7 @@ import styles from './styles.module.css';
 
 export const IntroTitle = () => {
 	return (
-		<section>
+		<section className={styles.introTitle}>
 			<h1>Маркетинг в данных и идеях</h1>
 			<a href="#">Запросить кейсы</a>
 		</section>
