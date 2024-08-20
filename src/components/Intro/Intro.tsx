b4076addaa74cbd06a7c216878ac9c62dfc74550
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-08-20 15:41:50 +0300

    feat: end section Intro without gradient

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,12 +1,14 @@
 import styles from './styles.module.css';
 import { IntroTitle } from './IntroTitle';
 import { Header } from './Header';
+import { Bublik } from './Bublik';
 
 export const Intro = () => {
 	return (
-		<section>
+		<section className={styles.intro}>
 			<Header />
 			<IntroTitle />
+			<Bublik />
 		</section>
 	)
 }
