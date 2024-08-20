05be677b867f95c57e5e2642ddda58c0b65bfe9f
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-08-20 11:46:30 +0300

    fix: margin and padding

--- src/components/Need/Need.tsx
+++ src/components/Need/Need.tsx
@@ -3,6 +3,7 @@ import styles from './styles.module.css';
 export const Need = () => {
   return (
     <div className={styles.needWrapper}>
+      <h1>fsdfsdafasd</h1>
       <div className={styles.title}>нам нужны</div>
       <div className={styles.section1}>
         <div className={styles.orangeSection}>
