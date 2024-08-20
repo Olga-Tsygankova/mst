033348e5e3fbb9cd01de91a1ed17d9cc09b6fe9c
sitl_idnm <klevtsevitch.lev@yandex.ru>
2024-08-20 16:41:34 +0300

    feat: complete Result component

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,11 +2,13 @@ import { Need } from '../../components/Need';
 import styles from './styles.module.css';
 import { HeartSection } from '../../components/HeartSection';
 import { Leader } from '../../components/Leader';
+import { Result } from '../../components/Result';
 
 export const MainPage = () => {
   return (
     <div className={styles.mainPageWrapper}>
       <Leader />
+      <Result />
       <Need />
       <HeartSection />
     </div>
