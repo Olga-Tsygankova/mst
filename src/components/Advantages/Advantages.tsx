e5a97ed3deb2b4412dd285d643ed91c63c7c4a3e
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-08-21 17:08:44 +0300

    feat: end section Advantages

--- src/components/Advantages/Advantages.tsx
+++ src/components/Advantages/Advantages.tsx
@@ -2,17 +2,17 @@ import styles from './styles.module.css';
 
 export const Advantages = () => {
 	return (
-		<section className={styles.advantades}>
-			<div className={styles.advantages__Content}>
-				<p className={styles.advantages__Title}>10</p>
+		<section className={styles.advantages}>
+			<div className={styles.advantagesContent}>
+				<p className={styles.advantages__title}>&#62;10</p>
 				<p className={styles.advantages__text}>лет опыта</p>
 			</div>
-			<div className={styles.advantagesContent}>
-				<p className={styles.advantages__Title}>85</p>
+			<div className={styles.advantagesContentPlanshet}>
+				<p className={styles.advantages__title}>85</p>
 				<p className={styles.advantages__text}>высококлассных специалистов</p>
 			</div>
 			<div className={styles.advantagesContent}>
-				<p className={styles.advantages__Title}>8</p>
+				<p className={styles.advantages__title}>8</p>
 				<p className={styles.advantages__text}>отделов для идеального результата</p>
 			</div>
 		</section>
