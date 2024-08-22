095e1877665c66125b172c39e8ecf18aaa866ae0
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-08-22 15:27:55 +0300

    init: end section Folders

--- src/components/Folders/Folders.tsx
+++ src/components/Folders/Folders.tsx
@@ -1,42 +1,75 @@
 import styles from './styles.module.css'
 import { FoldersTitle } from './FoldersTitle'
-import folder1 from '../../assets/Folders/folder1.svg'
-import folder2 from '../../assets/Folders/folder2.svg'
-import folder3 from '../../assets/Folders/folder3.svg'
-import folder4 from '../../assets/Folders/folder4.svg'
-import folder5 from '../../assets/Folders/folder5.svg'
-import folder6 from '../../assets/Folders/folder6.svg'
-import folder7 from '../../assets/Folders/folder7.svg'
-import folder8 from '../../assets/Folders/folder8.svg'
+import { Folder } from './Folder'
 
 export const Folders = () => {
 	return (
 		<section className={styles.folders}>
 			<FoldersTitle />
 			<div className={styles.foldersWrapper}>
-				<div className={styles.folder1}>
-					<img src={folder1} alt="" />
+				<div className={styles.folder}>
+					<div className={styles.folder1}>
+						<div className={styles.folderImg}>
+							<Folder />
+						</div>
+						<p>Отдел мобильной разработки</p>
+					</div>
 				</div>
-				<div className={styles.folder2}>
-					<img src={folder2} alt="" />
+				<div className={styles.folder}>
+					<div className={styles.folder2}>
+						<div className={styles.folderImg}>
+							<Folder />
+						</div>
+						<p>Креативный отдел</p>
+					</div>
 				</div>
-				<div className={styles.folder3}>
-					<img src={folder3} alt="" />
+				<div className={styles.folder}>
+					<div className={styles.folder3}>
+						<div className={styles.folderImg}>
+							<Folder />
+						</div>
+						<p>SЕО отдел</p>
+					</div>
 				</div>
-				<div className={styles.folder4}>
-					<img src={folder4} alt="" />
+				<div className={styles.folder}>
+					<div className={styles.folder4}>
+						<div className={styles.folderImg}>
+							<Folder />
+						</div>
+						<p>Отдел развития</p>
+					</div>
 				</div>
-				<div className={styles.folder5}>
-					<img src={folder5} alt="" />
+				<div className={styles.folder}>
+					<div className={styles.folder5}>
+						<div className={styles.folderImg}>
+							<Folder />
+						</div>
+						<p>HR отдел</p>
+					</div>
 				</div>
-				<div className={styles.folder6}>
-					<img src={folder6} alt="" />
+				<div className={styles.folder}>
+					<div className={styles.folder6}>
+						<div className={styles.folderImg}>
+							<Folder />
+						</div>
+						<p>IT отдел</p>
+					</div>
 				</div>
-				<div className={styles.folder7}>
-					<img src={folder7} alt="" />
+				<div className={styles.folder}>
+					<div className={styles.folder7}>
+						<div className={styles.folderImg}>
+							<Folder />
+						</div>
+						<p>Отдел медиабаинга</p>
+					</div>
 				</div>
-				<div className={styles.folder8}>
-					<img src={folder8} alt="" />
+				<div className={styles.folder}>
+					<div className={styles.folder8}>
+						<div className={styles.folderImg}>
+							<Folder />
+						</div>
+						<p>Финансовый отдел</p>
+					</div>
 				</div>
 			</div>
 		</section>
