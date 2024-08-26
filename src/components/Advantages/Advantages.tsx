48dcc2a9423ba5dc5aa400aaa6bf9f7c056be6f7
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-08-26 12:28:32 +0300

    fix: delete commit line

--- src/components/Advantages/Advantages.tsx
+++ src/components/Advantages/Advantages.tsx
@@ -4,20 +4,8 @@ import { AnimatedCard } from './AnimatedCard'
 export const Advantages = () => {
 	return (
 		<section className={styles.advantages}>
-			{/* <div className={styles.advantagesContent}>
-				<p className={styles.advantages__title}>&#62;10</p>
-				<p className={styles.advantages__text}>лет опыта</p>
-			</div>
-			<div className={styles.advantagesContentPlanshet}>
-				<p className={styles.advantages__title}>85</p>
-				<p className={styles.advantages__text}>высококлассных специалистов</p>
-			</div>
 			<div className={styles.advantagesContent}>
-				<p className={styles.advantages__title}>8</p>
-				<p className={styles.advantages__text}>отделов для идеального результата</p>
-			</div> */}
-			<div className={styles.advantagesContent}>
-				<AnimatedCard targetNumber={&10} text="лет опыта" index={0} />
+				<AnimatedCard targetNumber={10} text="лет опыта" index={0} />
 			</div>
 			<div className={styles.advantagesContent}>
 				<AnimatedCard targetNumber={85} text="высококлассных специалистов" index={1} />
