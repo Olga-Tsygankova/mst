8bc3e798a2d7921012e88cdf5a931b71c966f94f
Olga Tsygankova <tsygankova@ispolcom.ru>
2024-08-28 15:50:47 +0300

    feat: реализация скролла

--- src/components/HeartSection/HeartSection.tsx
+++ src/components/HeartSection/HeartSection.tsx
@@ -2,20 +2,49 @@ import styles from './styles.module.css';
 import { Arrow } from './Arrow';
 import { ArrowLine } from './ArrowLine';
 import { Heart } from './Heart';
-// import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 // import ScrollMagic from 'scrollmagic';
 
 export const HeartSection = () => {
+  const arrowRef = useRef<HTMLDivElement | null>(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (containerRef.current) {
+        const containerRect = containerRef.current.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+
+        if (containerRect.bottom < windowHeight / 2) {
+          setIsVisible(false);
+        } else {
+          setIsVisible(true);
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.container}>
+      <div className={styles.container} ref={containerRef}>
         <div className={styles.leftText}>
           <div>Удаленная работа</div>
           <div>Обучение, наставничество и поддержка</div>
           <div>Праздники и корпоративы в Дубае и других странах</div>
         </div>
         <div>
-          <div className={styles.arrow}>
+          <div
+            className={styles.arrow}
+            ref={arrowRef}
+            style={{ opacity: isVisible ? 1 : 0 }}
+          >
             <Arrow />
           </div>
           <div className={styles.arrowLine}>
