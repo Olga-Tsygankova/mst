4ed2ea0904d7db346549322d7a03a6e20deb1777
sitl_idnm <klevtsevitch.lev@yandex.ru>
2024-09-02 12:38:26 +0300

    feat: add tube functional

--- src/components/Hole/Hole.tsx
+++ src/components/Hole/Hole.tsx
@@ -4,8 +4,11 @@ import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import hole from '../../assets/Hole/hole.svg'
+import pipe from '../../assets/Tube/pipe.svg';
+import gradient from '../../assets/Tube/gradient.svg'
+import { MotionPathPlugin } from 'gsap/MotionPathPlugin';
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
 
 interface CardScrollAnimationProps {
   children: React.ReactNode;
@@ -58,9 +61,62 @@ const CardScrollAnimation = ({ children }: CardScrollAnimationProps) => {
   );
 };
 
+export const Tube: React.FC = () => {
+  const ballRef = useRef<HTMLDivElement>(null);
+  const pipeRef = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    if (ballRef.current && pipeRef.current) {
+      const path = pipeRef.current.querySelector('path');
+
+      if (path) {
+        gsap.timeline({
+          scrollTrigger: {
+            trigger: pipeRef.current,
+            start: 'top center',
+            end: 'bottom center',
+            scrub: true,
+            markers: true, // Удалите или оставьте для отладки
+          }
+        }).to(ballRef.current, {
+          motionPath: {
+            path: path,
+            align: path,
+            alignOrigin: [0.5, 0.5],
+            autoRotate: true,
+            start: 1,
+            end: 0
+          },
+          duration: 1,
+          ease: 'none'
+        });
+      }
+    }
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <img src={ pipe } className={styles.tube} alt="Pipe Icon" />
+      <svg ref={pipeRef} className={styles.pipe} viewBox="0 0 400 600">
+        <path
+          d="M6.82571 641C6.82571 641 -7.13588 502.341 8.92803 444.632C9.31885 443.21 9.73664 441.788 10.1679 440.415C33.7112 363.192 81.876 351.719 99.638 289.155C110.608 250.519 114.098 214.629 102.104 167.314C87.8057 110.929 45.7861 101.466 28.5498 70.331C9.96576 36.6469 10.2083 0.216919 10.2083 0.216919" // Замените путь, если нужно
+          fill="transparent"
+          stroke="transparent"
+          strokeWidth="2"
+        />
+      </svg>
+      <div ref={ballRef} className={styles.ball}>
+        {/* Замените этот div на вашу иконку */}
+        <img src={ gradient } alt="Ball Icon" />
+      </div>
+    </div>
+  );
+};
+
 export const Hole = () => {
   return (
     <CardScrollAnimation>
+      <Tube />
       <h2 className={styles.title}>Комплексно подходим к&nbsp;решению маркетинговых задач бизнеса</h2>
       <div className={styles.slider}>
         <div className={styles.cardWrapper}>
